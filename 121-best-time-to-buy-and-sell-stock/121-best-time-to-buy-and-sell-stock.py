4c9fc3b8464cd2ef193a9b86fd89c8a7e258a5b1
Manan <16bit122@nirmauni.ac.in>
2022-02-12 16:31:09 -0400

    Time: 1884 ms (16.39%), Space: 25.1 MB (66.96%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        if len(prices)==1:
+            return 0
+        
+        return self.solution(prices)
+        
+    def solution(self,prices):
+        ans = 0
+        size = len(prices)
+        left =0
+        right =1
+        
+        while left< size and right<size:
+            
+            if prices[right]<prices[left]:
+                left+=1
+            elif prices[right]-prices[left]>ans:
+                ans = prices[right]-prices[left]
+                right+=1
+            else:
+                right+=1
+        return ans
+        
+    def sol1(self,prices):
+        
+        ans = 0
+        for i in range(len(prices)):
+            
+            for j in range (i+1,len(prices)):
+                
+                if prices[j]-prices[i]>ans:
+                    ans = prices[j]-prices[i]
+        return ans
+    
