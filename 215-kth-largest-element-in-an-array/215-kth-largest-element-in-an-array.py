054e60277a0bc74a1d8d528f84cf5f9eac59fdbe
Manan <16bit122@nirmauni.ac.in>
2022-06-21 23:41:31 -0300

    Time: 182 ms (17.62%), Space: 14.9 MB (21.49%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -3,7 +3,24 @@ from queue import PriorityQueue
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        return self.another(nums,k)
+    
+    def another(self,nums,k):
         
+        pq = PriorityQueue()
+        
+        for index,num in enumerate(nums):
+            if index <k:
+                pq.put(num)
+            else:
+                temp = pq.get()
+                if temp> num:
+                    pq.put(temp)
+                else:
+                    pq.put(num)      
+        return pq.get()
+    
+    def naive(self,nums,k):
         pq = PriorityQueue() 
         
         for num in nums:
@@ -14,5 +31,4 @@ class Solution:
             ans = pq.get()
             
         return ans*-1
-            
         
